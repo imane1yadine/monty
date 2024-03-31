f4dadb5c70c2cb2196dc052ecfcac99e7fc5c578
imane1yadine <imanejeon4@gmail.com>
2024-03-31 10:41:20 -0700

    all

--- monty.h
+++ monty.h
@@ -8,9 +8,12 @@
 #include <string.h>
 #include <ctype.h>
 
+
+stack_t *add_node(stack_t **stack, const int n);
 void op_push(stack_t **stack, unsigned int counter);
 void op_pall(stack_t **stack, unsigned int counter);
 void op_pint(stack_t **stack, unsigned int counter);
-
+int execute(char *content, stack_t **stack, unsigned int counter, FILE *file);
+void free_stack(stack_t *stack);
 
 #endif /* monty.h */
